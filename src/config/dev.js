a8e1358fccfd15c6f777470ef5e0682e7b71061e
seif1000 <seifwin74@gmail.com>
2019-10-09 15:02:04 +0100

    confi

--- src/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    jwtSecret:"eeeee",
-    mongoURI:"mongodb://localhost/task-manger-react",
-    sendGridKey:"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",
-  
-  };
