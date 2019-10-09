13e154de47709d963af4d98c9be784c7e77c1213
seif1000 <seifwin74@gmail.com>
2019-10-09 19:26:25 +0100

    port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./src/app')
 const port = process.env.PORT
 
 app.listen(5000, () => {
-    console.log('Server is up on port ' + 5000)
+    console.log('Server is up on port ' + port);
 })
