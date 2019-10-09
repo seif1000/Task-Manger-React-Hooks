298a34c8b43eef4d01ddd190a2c00e22a1416bea
seif1000 <seifwin74@gmail.com>
2019-10-09 19:27:18 +0100

    port heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app')
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
