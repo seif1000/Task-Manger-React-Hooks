e3c98f70af77780930e7ec34d3f1c44f6058aeee
seif1000 <seifwin74@gmail.com>
2019-10-09 21:51:11 +0100

    laste steven

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -21,8 +21,16 @@ app.use(taskRouter)
 
 
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 module.exports = app
