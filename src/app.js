1656a4d52583e8975632b07939175029b936605d
seif1000 <seifwin74@gmail.com>
2019-10-09 21:46:20 +0100

    laste ahmed

--- src/app.js
+++ src/app.js
@@ -15,19 +15,14 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-if(process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, 'client/build'))); 
-      app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
-    }
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 module.exports = app
