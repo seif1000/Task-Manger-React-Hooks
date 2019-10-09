c15d58a8d9724dd080edc9f2dcc0ff4c7bd63b76
seif1000 <seifwin74@gmail.com>
2019-10-09 19:18:15 +0100

    var

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,7 +13,7 @@ router.post('/users', ensureGuest,async (req, res) => {
 //   
     try {
         await user.save()
-     //  sendWelcomeEmail(user.email, user.name)
+       sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -81,7 +81,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
-        //sendCancelationEmail(req.user.email, req.user.name)
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
