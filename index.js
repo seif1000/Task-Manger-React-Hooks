f62796022e48dd96c79322f94332d22e5e532637
seif1000 <seifwin74@gmail.com>
2019-10-10 01:12:57 +0100

    in

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const app = require('./src/app')
+const express = require('express')
+require('./src/db/mongoose');
+const userRouter = require('./src/routers/user');
+const taskRouter = require('./src/routers/task');
+const path = require('path');
+const cors= require('cors');
+
+
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(cors());
+
+
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
