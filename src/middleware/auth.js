9ca9aec527c619e45ea3067d79e58644f766f123
seif1000 <seifwin74@gmail.com>
2019-10-10 01:37:42 +0100

    fin

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,8 @@ const User = require('../models/user')
 const {jwtSecret} = require('../config/key');
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer ', '')
+       // const token = req.header('Authorization').replace('Bearer ', '')
+       const token =  req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, jwtSecret);
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
