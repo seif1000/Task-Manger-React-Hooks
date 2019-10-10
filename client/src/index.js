e4ea38c22baa275fd0445ec04eb7be601b0924b2
seif1000 <seifwin74@gmail.com>
2019-10-10 14:52:25 +0100

    unregister

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { unregister } from './registerServiceWorker';
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -10,3 +13,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
+unregister();
